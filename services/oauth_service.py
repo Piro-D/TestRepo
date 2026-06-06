380a023b716293d44e08cfe4e30feb35ce85f5fc
unknown <stevenimasjah@gmail.com>
2026-06-07 04:26:05 +0700

    Force HTTPS for Google OAuth redirect URI

--- services/oauth_service.py
+++ services/oauth_service.py
@@ -21,7 +21,7 @@ def get_client_config():
 def get_authorization_url():
     try:
         flow = Flow.from_client_config(get_client_config(), scopes=config.SCOPES)
-        flow.redirect_uri = url_for('oauth2callback', _external=True)
+        flow.redirect_uri = url_for('oauth2callback', _external=True).replace('http://', 'https://')
         authorization_url, state = flow.authorization_url(access_type='offline', include_granted_scopes='true')
         return authorization_url, state, flow.code_verifier
     except Exception as e:
@@ -30,7 +30,7 @@ def get_authorization_url():
 def handle_oauth_callback(authorization_response, state, code_verifier):
     try:
         flow = Flow.from_client_config(get_client_config(), scopes=config.SCOPES, state=state)
-        flow.redirect_uri = url_for('oauth2callback', _external=True)
+        flow.redirect_uri = url_for('oauth2callback', _external=True).replace('http://', 'https://')
         flow.code_verifier = code_verifier
         flow.fetch_token(authorization_response=authorization_response)
         return credentials_to_dict(flow.credentials)
